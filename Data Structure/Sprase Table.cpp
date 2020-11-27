ecb0cecdfc6c2b19e253eaee1ee190d87272c037
abc864197532 <65001169+abc864197532@users.noreply.github.com>
2020-11-28 01:54:06 +0800

    add sprase table

---
+++ Data Structure/Sprase Table.cpp
@@ -0,0 +1,24 @@
+const int N = 100000, logN = 17;
+
+int StMn[N][logN], StMx[N][logN];
+
+void buildSt() {
+    for (int j = 1; j < logN; ++j) {
+        for (int i = 0; i + (1 << j) <= N; ++i) {
+            StMn[i][j] = min(StMn[i][j - 1], StMn[i + (1 << j - 1)][j - 1]);
+            StMx[i][j] = max(StMx[i][j - 1], StMx[i + (1 << j - 1)][j - 1]);
+        }
+    }
+}
+
+int queryMn(int l, int r) {
+    if (r == l) return 1 << 30;
+    int g = __lg(r - l);
+    return min(StMn[l][g], StMn[r - (1 << g)][g]);
+}
+
+int queryMx(int l, int r) {
+    if (r == l) return -1;
+    int g = __lg(r - l);
+    return max(StMx[l][g], StMx[r - (1 << g)][g]);
+}
