c51402558da0313f44226b2bf0a6f92168798b3d
abc864197532 <65001169+abc864197532@users.noreply.github.com>
2021-02-10 19:09:43 +0800

    add FFT, NTT

--- Math/FFT.cpp
+++ Math/FFT.cpp
@@ -1,73 +1,41 @@
-/* p == (a << n) + 1  
-   g = pow(root, (p - 1) / n)
-   n    1<<n        p          a    root
-   5    32          97         3    5
-   6    64          193        3    5
-   7    128         257        2    3
-   8    256         257        1    3
-   9    512         7681       15   17
-   10   1024        12289      12   11
-   11   2048        12289      6    11
-   12   4096        12289      3    11
-   13   8192        40961      5    3
-   14   16384       65537      4    3
-   15   32768       65537      2    3
-   16   65536       65537      1    3
-   17   131072      786433     6    10
-   18   262144      786433     3    10 (605028353, 2308, 3)
-   19   524288      5767169    11   3
-   20   1048576     7340033    7    3
-   20   1048576     998244353  952  3
-   21   2097152     23068673   11   3
-   22   4194304     104857601  25   3
-   23   8388608     167772161  20   3
-   24   16777216    167772161  10   3
-   25   33554432    167772161  5    3 (1107296257, 33, 10)
-   26   67108864    469762049  7    3
-*/
+const long double pi = acos(-1), eps = 1e-14;
+typedef complex<long double> cp;
 
-const int mod = 998244353, N = 1 << 20;
- 
-long long modpow (long long a, int b) {
-    long long ans = 1;
-    for (; b; b >>= 1, a = a * a % mod) {
-        if (b & 1) ans = ans * a % mod;
+struct FFT {
+    int n, rev[N];
+    cp omega[N], iomega[N];
+    void init(int _n) {
+        n = _n;
+        for (int i = 0; i < n; ++i) {
+            omega[i] = cp(cos(pi * 2 / n * i), sin(pi * 2 / n * i));
+            iomega[i] = conj(omega[i]);
+        }
+        int k = __lg(n);
+        for (int i = 0; i < n; ++i) {
+            int t = 0;
+            for (int j = 0; j < k; ++j) {
+                if (i & (1 << j)) t |= (1 << k - j - 1);
+            }
+            rev[i] = t;
+        }
     }
-    return ans;
-}
- 
-const long long w = modpow(3, (mod - 1) / N);
- 
-void FFT (vector <long long> &P, bool inv = false) {
-    int n = P.size(), lg = __builtin_ctz(n);
-    
-    // bit reverse
-    for (int i = 0, j = 1; j < n - 1; ++j) {
-        int k = n >> 1;
-        while (k > (i ^= k)) k >>= 1;
-        if (i > j) swap(P[i], P[j]);
-    }
-    
-    vector <long long> ws = {inv ? modpow(w, mod - 2) : w};
-    for (int i = 1; i < lg; ++i) ws.push_back(ws.back() * ws.back() % mod);
-    reverse(ws.begin(), ws.end());
-    
-    for (int i = 0; i < lg; ++i) {
-        for (int k = 0; k < n; k += (2 << i)) {
-            long long base = 1;
-            for (int j = k; j < k + (1 << i); ++j, base = base * ws[i] % mod) {
-                long long t = base * P[j + (1 << i)] % mod, u = P[j];
-                P[j] = u + t;
-                if (P[j] >= mod) P[j] -= mod;
-                P[j + (1 << i)] = u - t;
-                if (P[j + (1 << i)] < 0) P[j + (1 << i)] += mod;
+    void transform(vector<cp> &a, cp *xomega) {
+        for (int i = 0; i < n; ++i) if (i < rev[i]) swap(a[i], a[rev[i]]);
+        for (int len = 2; len <= n; len <<= 1) {
+            int mid = len >> 1;
+            int r = n / len;
+            for (int j = 0; j < n; j += len) {
+                for (int i = 0; i < mid; ++i) {
+                    cp t = xomega[r * i] * a[j + mid + i];
+                    a[j + mid + i] = a[j + i] - t;
+                    a[j + i] += t;
+                }
             }
         }
     }
-    
-    if (inv) {
-        for (int i = 0; i < n; ++i) {
-            P[i] = P[i] * modpow(n, mod - 2) % mod;
-        }
+    void fft(vector<cp> &a) { transform(a, omega); }
+    void ifft(vector<cp> &a) {
+        transform(a, iomega);
+        for (int i = 0; i < n; ++i) a[i] /= n;
     }
-}
+};
