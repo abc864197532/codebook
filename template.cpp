142ba5a4446dd7f4438a3d13417d75cd1f4e2630
abc864197532 <65001169+abc864197532@users.noreply.github.com>
2021-04-21 08:35:51 +0800

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define test(x) cout << "Line(" << __LINE__ << ") " #x << ' ' << x << endl
+#define printv(x) {\
+	for (auto i : x) cout << i << ' ';\
+	cout << endl;\
+}
+#define pii pair <int, int>
+#define pll pair <lli, lli>
+#define X first
+#define Y second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+template<typename A, typename B>
+ostream& operator << (ostream& o, pair<A, B> a){
+    return o << a.X << ' ' << a.Y;
+}
+template<typename A, typename B>
+istream& operator >> (istream& o, pair<A, B> &a){
+    return o >> a.X >> a.Y;
+}
+const int mod = 1e9 + 7, abc = 864197532, Doludu = 123, N = 300001, K = 111;
