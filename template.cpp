a3c59e894c62203e7ff0fcbef2818110009a9682
abc864197532 <65001169+abc864197532@users.noreply.github.com>
2021-04-29 23:45:56 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,23 +4,28 @@ using namespace std;
 #define mp make_pair
 #define pb push_back
 #define eb emplace_back
-#define test(x) cout << "Line(" << __LINE__ << ") " #x << ' ' << x << endl
-#define printv(x) {\
-	for (auto i : x) cout << i << ' ';\
-	cout << endl;\
-}
 #define pii pair <int, int>
-#define pll pair <lli, lli>
 #define X first
 #define Y second
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
-template<typename A, typename B>
-ostream& operator << (ostream& o, pair<A, B> a){
-    return o << a.X << ' ' << a.Y;
+#define info() cerr << __PRETTY_FUNCTION__ << ": " << __LINE__ << endl
+#define test(args...) info(), kout("[" + string(#args) + "]", args)
+void kout() {cerr << endl;}
+template <typename T, class ...U>
+void kout(vector <T> a, U ...b) {for (T i : a) cerr << i << ' '; kout(b...);}
+template <class T, class ...U>
+void kout(T a, U ...b) {cerr << a << ' ', kout(b...);}
+template <class T>
+void printv(T l, T r) {
+    while (l != r) cout << *l << " \n"[++l == r];
 }
-template<typename A, typename B>
-istream& operator >> (istream& o, pair<A, B> &a){
+template <typename A, typename B>
+istream& operator >> (istream& o, pair<A, B> &a) {
     return o >> a.X >> a.Y;
 }
-const int mod = 1e9 + 7, abc = 864197532, Doludu = 123, N = 300001, K = 111;
+template <typename A, typename B>
+ostream& operator << (ostream& o, pair<A, B> a) {
+    return o << a.X << ' ' << a.Y;
+}
+const int mod = 1e9 + 7, abc = 864197532, Doludu = 123, N = 400001, K = 111;
