ea81ba95a22f88cda9f367e62041e02c1a8a1df2
abc864197532 <65001169+abc864197532@users.noreply.github.com>
2021-04-30 15:14:52 +0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -12,20 +12,21 @@ using namespace std;
 #define info() cerr << __PRETTY_FUNCTION__ << ": " << __LINE__ << endl
 #define test(args...) info(), kout("[" + string(#args) + "]", args)
 void kout() {cerr << endl;}
-template <typename T, class ...U>
-void kout(vector <T> a, U ...b) {for (T i : a) cerr << i << ' '; kout(b...);}
-template <class T, class ...U>
-void kout(T a, U ...b) {cerr << a << ' ', kout(b...);}
-template <class T>
-void printv(T l, T r) {
+template <typename T, typename ...U> void kout(T a, U ...b) {
+    cerr << a << ' ', kout(b...);
+}
+template <typename T> void printv(T l, T r) {
     while (l != r) cout << *l << " \n"[++l == r];
 }
-template <typename A, typename B>
-istream& operator >> (istream& o, pair<A, B> &a) {
+template <typename A, typename B> istream& operator >> (istream& o, pair<A, B> &a) {
     return o >> a.X >> a.Y;
 }
-template <typename A, typename B>
-ostream& operator << (ostream& o, pair<A, B> a) {
-    return o << a.X << ' ' << a.Y;
+template <typename A, typename B> ostream& operator << (ostream& o, pair<A, B> a) {
+    return o << '(' << a.X << ", " << a.Y << ')';
+}
+template <typename T> ostream& operator << (ostream& o, vector<T> a) {
+    bool is = false;
+    for (T i : a) {o << (is ? ' ' : '{'), is = true, o << i;}
+    return o << '}';
 }
 const int mod = 1e9 + 7, abc = 864197532, Doludu = 123, N = 400001, K = 111;
