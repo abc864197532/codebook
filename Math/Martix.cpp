d01334a987cac63c701ece6bebc02d7eb3393784
abc864197532 <65001169+abc864197532@users.noreply.github.com>
2021-06-10 17:59:33 +0800

    Update Martix.cpp

--- Math/Martix.cpp
+++ Math/Martix.cpp
@@ -1,9 +1,8 @@
-const int mod = 1e9 + 7;  
-
 struct mat {
     int n;
+    long long mod2;
     vector <vector <long long>> val;
-    mat (int _n, int _mode = 0) : n(_n) {
+    mat (int _n, int _mode = 0) : n(_n), mod2(1ll * mod * mod) {
         // mode 1 -> I, other -> O
         val.assign(n, vector<long long>(n, 0));
         if (_mode == 1) {
@@ -23,7 +22,8 @@ struct mat {
         mat ans(o);
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                ans.val[i][j] = (ans.val[i][j] + val[i][j]) % mod;
+                ans.val[i][j] += val[i][j];
+                if (ans.val[i][j] >= mod) ans.val[i][j] -= mod;
             }
         }
         return ans;
@@ -31,10 +31,16 @@ struct mat {
     mat operator * (const mat& o) {
         mat ans(n, 0);
         for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                for (int k = 0; k < n; ++k) {
-                    ans.val[i][j] = (ans.val[i][j] + val[i][k] * o.val[k][j]) % mod;
+            for (int k = 0; k < n; ++k) {
+                for (int j = 0; j < n; ++j) {
+                    ans.val[i][j] += val[i][k] * o.val[k][j];
+                    if (ans.val[i][j] >= mod2) ans.val[i][j] -= mod2;
                 }
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                ans.val[i][j] %= mod;
             }
         }
         return ans;
