e4c8ef54e89f51e4c5b4578bcd3ddc4ee0e991c6
abc864197532 <kimi930130@gmail.com>
2022-07-10 23:19:43 +0800

    upd

--- Tree/VirtualTree.cpp
+++ Tree/VirtualTree.cpp
@@ -3,12 +3,12 @@ vector <int> newadj[N];
 vector <int> build_virtual_tree(vector <int> a) {
     // need lca
     int sz = a.size();
-    sort(all(a), [&](int i, int j) {return in[i] < in[j];});
-    for (int i = 1; i < sz; ++i) {a.pb(lca(a[i - 1], a[i]));}
-    sort(all(a), [&](int i, int j) {return in[i] < in[j];});
+    sort(all(a), [&](int i, int j) {return LCA.in[i] < LCA.in[j];});
+    for (int i = 1; i < sz; ++i) {a.pb(LCA.lca(a[i - 1], a[i]));}
+    sort(all(a), [&](int i, int j) {return LCA.in[i] < LCA.in[j];});
     a.resize(unique(a.begin(), a.end()) - a.begin());
     for (int i = 1; i < a.size(); ++i) {
-        int u = lca(a[i - 1], a[i]), v = a[i];
+        int u = LCA.lca(a[i - 1], a[i]), v = a[i];
         newadj[u].push_back(v), newadj[v].push_back(u);
     }
     return a;
