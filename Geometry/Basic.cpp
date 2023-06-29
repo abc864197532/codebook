2f2c548da1d7daaa17c9ba1bb82436f8308397a5
abc864197532 <kimi930130@gmail.com>
2023-06-29 22:32:11 +0800

    update geometry basic

--- Geometry/Basic.cpp
+++ Geometry/Basic.cpp
@@ -1,35 +1,35 @@
+int sign(ll x) {return abs(x) == 0 ? 0 : (x > 0 ? 1 : -1);}
 struct Pt {
-    long long x, y;
-    Pt (long long _x = 0, long long _y = 0) : x(_x), y(_y) {}
-    long long operator * (const Pt &o) {
-        return x * o.x + y * o.y;
-    }
-    long long operator ^ (const Pt &o) {
-        return x * o.y - y * o.x;
-    }
-    Pt operator + (const Pt &o) {
-        return Pt(x + o.x, y + o.y);
-    }
-    Pt operator - (const Pt &o) {
-        return Pt(x - o.x, y - o.y);
-    }
-    bool pos() {
-        return (y == 0 ? x < 0 : y < 0);
-    }
-    long long dis() {
-        return x * x + y * y;
-    }
+  ll x, y;
+  Pt (ll _x, ll _y) : x(_x), y(_y) {}
+  Pt operator + (Pt o) {return Pt(x + o.x, y + o.y);}
+  Pt operator - (Pt o) {return Pt(x - o.x, y - o.y);}
+  Pt operator * (ll k) {return Pt(x * k, y * k);}
+  Pt operator / (ll k) {return Pt (x / k, y / k);}
+  ll operator * (Pt o) {return x * o.x + y * o.y;}
+  ll operator ^ (Pt o) {return x * o.y - y * o.x;}
 };
-
-bool btw(Pt a, Pt b, Pt c) {
-    // c in segment ab?
-    return ((a - b) ^ (a - c)) == 0 && ((c - a) * (c - b)) <= 0;
+struct Line {
+  Pt a, b;
+};
+struct Cir {
+  Pt o; ll r;
+};
+ll abs2(Pt o) {return o.x * o.x + o.y * o.y;}
+double abs(Pt o) {return sqrt(abs2(o));}
+int ori(Pt o, Pt a, Pt b) {return sign((o - a) ^ (o - b));}
+bool btw(Pt a, Pt b, Pt c) { // c on segment ab?
+  return ori(a, b, c) == 0 && sign((c - a) * (c - b)) <= 0;
 }
-
-long long area(Pt a, Pt b, Pt c) {
-    return abs((a - b) ^ (a - c));
+double area(Pt a, Pt b, Pt c) {return abs((a - b) ^ (a - c)) / 2;}
+Pt unit(Pt o) {return o / abs(o);}
+Pt rot(Pt a, double o) { // CCW
+  double c = cos(o), s = sin(o);
+  return Pt(c * a.x - s * a.y, s * a.x + c * a.y);
 }
-
-double dis(Pt a, Pt b, Pt c) {
-    return double(area(a, b, c)) / sqrt((b - c).dis());
+Pt proj_vector(Pt a, Pt b, Pt c) { // vector ac proj to ab
+  return (b - a) * ((c - a) * (b - a)) / ((b - a) * (b - a));
+}
+Pt proj_pt(Pt a, Pt b, Pt c) { // point c proj to ab
+  return proj_vector(a, b, c) + a;
 }
